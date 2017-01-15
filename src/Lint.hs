bb5195548cf1db44ea81cd72ab80fe09d550663d
shawa <andrewshaw.mail@gmail.com>
2017-01-15 22:57:04 +0000

    Clean up linter a tad

--- src/Lint.hs
+++ src/Lint.hs
@@ -1,9 +1,15 @@
 module Lint where
 import Control.Monad.Writer
-import Data.List ((\\), intercalate)
+import Data.List (intercalate)
 import Statement
 import Expression (Name, Expr(..))
 
+-- Quick 'n dirty! Unused variables are any variable that's in an assignment
+-- statement, but not in any expression. As a proof-of-concept, we do directly
+-- this, and first make a pass over the program to get any defined variable names,
+-- then do a second pass and get all of the variable names that appear in an
+-- expression. The difference (\\) of these two lists is then all of the
+-- declared, but unused variables.
 usedVars :: Statement -> [Name]
 usedVars (s1 :. s2 )    = usedVars s1 ++ usedVars s2
 usedVars (Print e)      = getVars e
@@ -25,22 +31,14 @@ getVars (Gt  e1 e2) = getVars e1 ++ getVars e2
 getVars (Lt  e1 e2) = getVars e1 ++ getVars e2
 getVars (Const _)   = mempty
 
-
 declaredVars :: Statement -> [Name]
 declaredVars (s1 :. s2)      = declaredVars s1 ++ declaredVars s2
 declaredVars (Assign name _) = [name]
 declaredVars _               = []
 
-
-unusedVars :: Statement -> [Name]
-unusedVars stmt = let declared = declaredVars stmt
-                      used     = usedVars     stmt in
-                  declared \\ used
-
-
 unusedVarMessage :: Statement -> Maybe String
-unusedVarMessage stmt = go $ unusedVars stmt
-    where go [] = Nothing
+unusedVarMessage stmt = go $ declaredVars stmt `less` usedVars stmt
+    where xs `less` ys = [x | x <- xs, x `notElem` ys]
+          go [] = Nothing
           go [x] = Just $! "Yikes, variable " ++ x ++ " is defined but never used!"
-          go xs = Just $! "Great Scott! The variables "  ++ vars ++ " are defined, but never used!"
-                  where vars = intercalate ", " xs
+          go xs  = Just $! "Great Scott! The variables "  ++ (intercalate ", " xs) ++ " are defined, but never used!"
