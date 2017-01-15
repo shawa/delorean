21188aa4d1556b4d46e3ea61d55028bcae543de3
shawa <andrewshaw.mail@gmail.com>
2017-01-15 17:49:58 +0000

    Add prints

---
+++ testPrograms/prints.dl.hs
@@ -0,0 +1,9 @@
+[ (Print (Const (I 1)))
+, (Print (Const (I 2)))
+, (Print (Const (I 3)))
+, (Print (Const (I 4)))
+, (Print (Const (I 5)))
+, (Print (Const (I 6)))
+, (Print (Const (I 7)))
+, (Print (Const (I 8)))
+]
